501289423d344c17665e4f662177e0bfed741e47
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-04-18 07:49:52 -0400

    Safeguard against integer overflow for the wall height

--- src/draw_frame.c
+++ src/draw_frame.c
@@ -16,6 +16,8 @@ void draw_frame(const struct draw_info *info,
 		vec_rotate(&dpos, info->fov * ((double)i / info->n_rays - 0.5));
 		wall = cast_ray(map, pos, &dpos, &len);
 		wall_height = info->wall_half / len * info->height / 2; 
+		if (wall_height < 0 || wall_height > info->height)
+			wall_height = info->height;
 		padding = (info->height - wall_height) / 2;
 		for (y = 0; y < padding; ++y) {
 			mvaddch(y, i, ' ');
